ab2129e4068fefaa33d0abe29b4d4c29ed06acb0
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2016-09-09 18:25:42 +0200

    update utente da finire

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -9,12 +9,13 @@
 class UserController extends Controller{
     
     public $layout='column2';
+    private $_model;
     
     public function accessRules()
 	{
 		return array(
 			array('allow',  // allow all users to access 'index' and 'view' actions.
-				'actions'=>array('lista','dettaglio'),
+				'actions'=>array('lista','dettaglio','update'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
@@ -35,10 +36,25 @@ class UserController extends Controller{
     public function actionDettaglio()
     {
        if (isset($_GET['id']))
+       {
+           $utente = User::model()->findByPk($_GET['id']);
+           $this->_model=User::model()->findByPk($_GET['id']);}
 
-
-        $utente = User::model()->findByPk($_GET['id']);
+           else if (isset($_POST['User']))
+        {
+            $model = $this->_model;
+            var_dump($model);
+            $model->attributes = $_POST['User'];
+            if($model->save())
+                $this->redirect(array('listaUtenti'));
+       }
         $this->render('dettaglio',array('utente' => $utente));
+    }
+
+    public function actionUpdate()
+    {
+//        $test = $_POST['User'];
+//        $this->render('test',array('test'=>$test));
     }
     
     
--- protected/views/user/dettaglio.php
+++ protected/views/user/dettaglio.php
@@ -1,20 +1,24 @@
-<form action='/blog/user/update' method='post'>
+<form action='/blog/user/dettaglio' method='post' >
     
   <div class="form-group">
     <label for="formGroupExampleInput">Username</label>
-    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username" value="<?php echo $utente->username ?>">
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Username" name="User[username]" value="<?php echo $utente->username ?>">
   </div>
   <div class="form-group">
     <label for="formGroupExampleInput2">Email</label>
-    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email" value="<?php echo $utente->email ?>">
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email" name="User[email]" value="<?php echo $utente->email ?>">
+  </div>
+  <div class="form-group">
+    <label for="formGroupExampleInput2">Password</label>
+    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email" name="User[password]" value="<?php echo $utente->password ?>">
   </div>
   <div class="form-group">
     <label for="formGroupExampleInput2">Profilo</label>
-    <textarea class="form-control" id="formGroupExampleInput2">
+    <textarea class="form-control" name="User[profile]" id="formGroupExampleInput2">
         <?php echo $utente->profile ?>
     </textarea>
   </div>
     
-    <button type="button" class="btn btn-primary">Salva</button>
+    <button type="submit" class="btn btn-primary">Salva</button>
    
 </form>
