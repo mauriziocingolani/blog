7d9003ccc52d8704778cccf18305b55dbc9237b0
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2016-09-09 14:54:26 +0200

    table

--- protected/views/user/_utente.php
+++ protected/views/user/_utente.php
@@ -1,3 +1,14 @@
-<div> <?php echo $data->username ?> </div></br>
-<div> <?php echo $data->email ?> </div></br>
-<div> <?php echo $data->profile ?> </div></br>
+<table class="table">
+    <thead class="thead-inverse">
+    <tr>
+        <th>Username</th>
+        <th>Email</th>
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $data->username ?></td>
+            <td><?php echo $data->email ?> </td>
+        </tr>
+    </tbody>
+</table>
