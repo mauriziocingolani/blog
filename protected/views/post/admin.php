06917f74c964f3f5ab92e484a3184267e4746ba5
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2016-09-13 10:59:26 +0200

    cambiato bottone su manage post

--- protected/views/post/admin.php
+++ protected/views/post/admin.php
@@ -24,6 +24,11 @@ $this->breadcrumbs=array(
 		),
 		array(
 			'class'=>'CButtonColumn',
+			'buttons'=>array(
+				'delete'=>array(
+				'label'=>'Delete',
+				'url'=>'Yii::app()->createUrl("post/delete",array("id"=>$data->id,"title"=>$data->title))',
+				'imageUrl'=>'https://linearicons.com/free/icon/lnr-trash.svg')),
 		),
 	),
 )); ?>
